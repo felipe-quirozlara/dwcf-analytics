50cd619fd480ad1c4f513b1edd74b304a40d9e7d
Felipe Quiroz <47114103+felipe-quirozlara@users.noreply.github.com>
2024-05-07 17:59:18 -0400

    new release demo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-localStorage.setItem("demo", "holaaa")
+import { algunaConfig } from "./module/config.js"
+
+const data = algunaConfig;
+
+function createData(data){
+  localStorage.setItem("data", data)
+}
